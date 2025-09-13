8bcb6c0a7e692463a5729fbcfca06c12eca40f2c
DHARWIN S <155514086+Dharwin77@users.noreply.github.com>
2025-09-13 18:33:48 +0530

    Update NQueens.java

--- NQueens.java
+++ NQueens.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List
+import java.util.List;
 import java.util.Scanner;
 
 public class NQueens {
@@ -119,4 +119,5 @@ public class NQueens {
         }
     }
 }
+
 
